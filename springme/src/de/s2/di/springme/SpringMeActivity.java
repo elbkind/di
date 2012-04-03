d0b125e0bdd3ac436eae86743d5ab78262759181
Mark Vollmann <mark.vollmann@web.de>
2012-04-03 21:24:54 +0200

    initial

---
+++ springme/src/de/s2/di/springme/SpringMeActivity.java
@@ -0,0 +1,15 @@
+package de.s2.di.springme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SpringMeActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
